a1e3c7ada360e99e5a1341b1b7cbfac49807a415
GrantThorntonIE <145786343+GrantThorntonIE@users.noreply.github.com>
2023-12-07 13:20:03 +0000

    Update function_app.py
    output xml as string 's'

--- function_app.py
+++ function_app.py
@@ -653,7 +653,7 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
         json_data = json.dumps({
             'email' : email,
             'name'  : plan_name, 
-            'table' : output
+            'table' : s
         })
 
         local_file_name = str(uuid.uuid4()) + '.json'
