1584f75f427413619a352ebb9ac356c7c5e9f4e8
GrantThorntonIE <145786343+GrantThorntonIE@users.noreply.github.com>
2024-01-16 22:39:47 +0000

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -60,7 +60,7 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
         # xml = req._HttpRequest__body_str
         # xml = req.get_json()
         
-        output = req.data
+        output = str(req)
         sc = 200    # OK
 
     except Exception as ex:
