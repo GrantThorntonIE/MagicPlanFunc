dbeb067e7f63daa52e1169205ce0e873d04df690
GrantThorntonIE <145786343+GrantThorntonIE@users.noreply.github.com>
2024-01-18 20:03:47 +0000

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -59,17 +59,65 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
         email = 'RPASupport@ie.gt.com'
         
 
-        # output = {}
-        # req_dir = dir(req)
-        # for m in req_dir:
-        #     x = hasattr(m, '__getitem__')
-        #     output[m] = x
+        output = {}
+        output['_HttpRequest__body_bytes'] = hasattr(req._HttpRequest__body_bytes, '__getitem__')
+        output['_HttpRequest__body_str'] = hasattr(req._HttpRequest__body_str, '__getitem__')
+        output['_HttpRequest__body_type'] = hasattr(req._HttpRequest__body_type, '__getitem__')
+        output['_HttpRequest__files'] = hasattr(req._HttpRequest__files, '__getitem__')
+        output['_HttpRequest__form'] = hasattr(req._HttpRequest__form, '__getitem__')
+        output['_HttpRequest__form_parsed'] = hasattr(req._HttpRequest__form_parsed, '__getitem__')
+        output['_HttpRequest__headers'] = hasattr(req._HttpRequest__headers, '__getitem__')
+        output['_HttpRequest__method'] = hasattr(req._HttpRequest__method, '__getitem__')
+        output['_HttpRequest__params'] = hasattr(req._HttpRequest__params, '__getitem__')
+        output['_HttpRequest__route_params'] = hasattr(req._HttpRequest__route_params, '__getitem__')
+        output['_HttpRequest__url'] = hasattr(req._HttpRequest__url, '__getitem__')
+        output['__abstractmethods__'] = hasattr(req.__abstractmethods__, '__getitem__')
+        output['__annotations__'] = hasattr(req.__annotations__, '__getitem__')
+        output['__class__'] = hasattr(req.__class__, '__getitem__')
+        output['__delattr__'] = hasattr(req.__delattr__, '__getitem__')
+        output['__dict__'] = hasattr(req.__dict__, '__getitem__')
+        output['__dir__'] = hasattr(req.__dir__, '__getitem__')
+        output['__doc__'] = hasattr(req.__doc__, '__getitem__')
+        output['__eq__'] = hasattr(req.__eq__, '__getitem__')
+        output['__format__'] = hasattr(req.__format__, '__getitem__')
+        output['__ge__'] = hasattr(req.__ge__, '__getitem__')
+        output['__getattribute__'] = hasattr(req.__getattribute__, '__getitem__')
+        output['__gt__'] = hasattr(req.__gt__, '__getitem__')
+        output['__hash__'] = hasattr(req.__hash__, '__getitem__')
+        output['__init__'] = hasattr(req.__init__, '__getitem__')
+        output['__init_subclass__'] = hasattr(req.__init_subclass__, '__getitem__')
+        output['__le__'] = hasattr(req.__le__, '__getitem__')
+        output['__lt__'] = hasattr(req.__lt__, '__getitem__')
+        output['__module__'] = hasattr(req.__module__, '__getitem__')
+        output['__ne__'] = hasattr(req.__ne__, '__getitem__')
+        output['__new__'] = hasattr(req.__new__, '__getitem__')
+        output['__reduce__'] = hasattr(req.__reduce__, '__getitem__')
+        output['__reduce_ex__'] = hasattr(req.__reduce_ex__, '__getitem__')
+        output['__repr__'] = hasattr(req.__repr__, '__getitem__')
+        output['__setattr__'] = hasattr(req.__setattr__, '__getitem__')
+        output['__sizeof__'] = hasattr(req.__sizeof__, '__getitem__')
+        output['__slots__'] = hasattr(req.__slots__, '__getitem__')
+        output['__str__'] = hasattr(req.__str__, '__getitem__')
+        output['__subclasshook__'] = hasattr(req.__subclasshook__, '__getitem__')
+        output['__weakref__'] = hasattr(req.__weakref__, '__getitem__')
+        output['_abc_impl'] = hasattr(req._abc_impl, '__getitem__')
+        output['_parse_form_data'] = hasattr(req._parse_form_data, '__getitem__')
+        output['files'] = hasattr(req.files, '__getitem__')
+        output['form'] = hasattr(req.form, '__getitem__')
+        output['get_body'] = hasattr(req.get_body, '__getitem__')
+        output['get_json'] = hasattr(req.get_json, '__getitem__')
+        output['headers'] = hasattr(req.headers, '__getitem__')
+        output['method'] = hasattr(req.method, '__getitem__')
+        output['params'] = hasattr(req.params, '__getitem__')
+        output['route_params'] = hasattr(req.route_params, '__getitem__')
+        output['url'] = hasattr(req.url, '__getitem__')
 
 
+
         
         
         
-        output = req.get_body().decode('utf-8')
+        # output = req.get_body().decode('utf-8')
         
         
 
