bccf9fb8e113e0541e0bc004ce6588d0c608d635
GrantThorntonIE <145786343+GrantThorntonIE@users.noreply.github.com>
2024-05-24 02:14:26 +0100

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -2137,7 +2137,7 @@ def survey(root):
         print('json_val_dict["Is a Major Renovation calculation necessary?*"]', ':', json_val_dict["Is a Major Renovation calculation necessary?*"])
         if json_val_dict["Is a Major Renovation calculation necessary?*"] == True:
             print('generating template_mrc')
-            populate_template_new(output_dict, 'template_mrc')
+            output = populate_template_new(output_dict, 'template_mrc')
         
         # print(ofl_wos)
         
@@ -2156,38 +2156,38 @@ def survey(root):
         
         
         # print('Attic (Loft) Insulation 200 mm top-up', ':', json_val_dict['Attic (Loft) Insulation 200 mm top-up'])
-
-        styling = "border=\"1\""
-        output = f"""\
-            <h1>Work Order Summary</h1> \
-            {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_wos)} \
-            <h1>General</h1> \
-            {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_general)} \
-            <h1>Major Renovation</h1> \
-            {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_mr)} \
-            <h1>Primary Measures</h1> \
-            {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_pm)} \
-            <h1>Roof</h1> \
-            {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_roof)} \
-            <h1>Walls</h1> \
-            {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_walls)} \
-            <h1>Heating</h1> \
-            {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_heating)} \
-            <h1>Mechanical Ventilation Systems, Air Tightness Testing & Energy</h1> \
-            {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_mae)} \
-            <h1>Supplementary</h1> \
-            {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_s)} \
-            <h1>Heating Primary Measures</h1> \
-            {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_hpm)} \
-
-            </div>"""
-
-
-        if warnings != '':
+        if output != '':
+            styling = "border=\"1\""
             output = f"""\
-                <h1>Warnings</h1> \
-                {warnings} \
-                </div>""" + output
+                <h1>Work Order Summary</h1> \
+                {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_wos)} \
+                <h1>General</h1> \
+                {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_general)} \
+                <h1>Major Renovation</h1> \
+                {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_mr)} \
+                <h1>Primary Measures</h1> \
+                {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_pm)} \
+                <h1>Roof</h1> \
+                {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_roof)} \
+                <h1>Walls</h1> \
+                {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_walls)} \
+                <h1>Heating</h1> \
+                {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_heating)} \
+                <h1>Mechanical Ventilation Systems, Air Tightness Testing & Energy</h1> \
+                {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_mae)} \
+                <h1>Supplementary</h1> \
+                {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_s)} \
+                <h1>Heating Primary Measures</h1> \
+                {create_table_text(output_dict, headers = ['name', 'value'], styling=styling, do_not_sum=['All'], order_list = ofl_hpm)} \
+
+                </div>"""
+
+
+            if warnings != '':
+                output = f"""\
+                    <h1>Warnings</h1> \
+                    {warnings} \
+                    </div>""" + output
 
         # print(output)
 
@@ -2419,7 +2419,7 @@ def azure_upload(file_data, container_name = 'attachment'):
 
 def populate_template_new(json_val_dict, template):
     try:
-    
+        output = ''
         if template == 'template':
             filename = json_val_dict['plan_name'] + '.xlsx'
             container_name = 'attachment'
@@ -2759,7 +2759,7 @@ def populate_template_new(json_val_dict, template):
         print(output)
 
     finally:
-        return instance_file_path
+        return output
 
 
 
